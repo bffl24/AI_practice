bff579fe76c062b27d338c6142c03e6fc2e2be1e
Udayan Panda <144189584+bffl24@users.noreply.github.com>
2026-02-10 17:46:48 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,49 @@
+<TEMPLATE>
+      **Here is the aggregated Member data for Subscriber ID <subscriber_id>:**
+      
+      Member Name: <first_name> <last_name>
+      Member Date of Birth: <birth_date>
+      Member Age: <age>
+      Subscriber ID: <subscriber_id>
+
+      **Contact Information:**
+      &nbsp;&nbsp;<contact_source>:
+      &nbsp;&nbsp;&nbsp;&nbsp;<phone_1>
+
+      **Current Situation and Diagnosis (Last 120 Days, MHK Notes):**
+      <current_situation_text>
+
+      **Current Medications (Last 120 Days):**
+      &nbsp;&nbsp;CVS: 
+      &nbsp;&nbsp;&nbsp;&nbsp;<cvs_text>
+      &nbsp;&nbsp;MHK Self Reporting: 
+      &nbsp;&nbsp;&nbsp;&nbsp;<mhk_text>
+
+      **Most recent ER Visit and Hospitalization dates:**
+
+      &nbsp;&nbsp;MHK Notes:
+      &nbsp;&nbsp;&nbsp;&nbsp;Hospitalization: <mhk_hosp_start_date> - <mhk_hosp_end_date>
+      &nbsp;&nbsp;&nbsp;&nbsp;Provider: <mhk_hosp_provider>
+      &nbsp;&nbsp;&nbsp;&nbsp;Diagnosis:
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<mhk_hosp_dx_1>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<mhk_hosp_dx_2>
+
+      &nbsp;&nbsp;&nbsp;&nbsp;ER Visit: <mhk_er_start_date> - <mhk_er_end_date>
+      &nbsp;&nbsp;&nbsp;&nbsp;Provider: <mhk_er_provider>
+      &nbsp;&nbsp;&nbsp;&nbsp;Diagnosis:
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<mhk_er_dx_1>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<mhk_er_dx_2>
+
+      &nbsp;&nbsp;Claims Payment API:
+      &nbsp;&nbsp;&nbsp;&nbsp;Hospitalization: <hosp_info>
+      &nbsp;&nbsp;&nbsp;&nbsp;Provider: <hosp_provider>
+      &nbsp;&nbsp;&nbsp;&nbsp;ICD-10 Primary Diagnosis Code: <hosp_dx_code>
+      &nbsp;&nbsp;&nbsp;&nbsp;ICD-10 Primary Diagnosis Code Description: <hosp_dx_desc>
+
+      &nbsp;&nbsp;&nbsp;&nbsp;ER Visit: <er_info>
+      &nbsp;&nbsp;&nbsp;&nbsp;Provider: <er_provider>
+      &nbsp;&nbsp;&nbsp;&nbsp;ICD-10 Primary Diagnosis Code: <er_dx_code>
+      &nbsp;&nbsp;&nbsp;&nbsp;ICD-10 Primary Diagnosis Code Description: <er_dx_desc>
+
+      **Caution:** Please verify this information before use.
+</TEMPLATE>
