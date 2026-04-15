c793c90c45b4280864928c8eb6b513dbb228a392
Udayan Panda <144189584+bffl24@users.noreply.github.com>
2026-04-15 14:28:50 +0530

    Create strarter.py

---
+++ strarter.py
@@ -0,0 +1,5 @@
+export PORTKEY_API_KEY="your_portkey_api_key"
+export PORTKEY_VIRTUAL_KEY="your_virtual_key"
+export PORTKEY_MODEL="gpt-4.1-mini"
+export USE_PORTKEY="1"
+python aisdlc_solution_design_agent.py
